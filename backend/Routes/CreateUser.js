65be51b6a088f5a090d084a9c4874fddebf82183
Anupam Dwivedi <anupamdwivedi2270@gmail.com>
2023-07-11 02:30:45 +0530

    create user update

--- backend/Routes/CreateUser.js
+++ backend/Routes/CreateUser.js
@@ -5,12 +5,18 @@ const User = require('../models/User')
 
 router.post("/createuser", async (req,res) => {
     try{
+        // await User.create({
+        //     name:"Anupam", // req.body.name -> use it for getting data from user  
+        //     password:"123",
+        //     email:"abc12@gmail.com",
+        //     location:"sada"
+        // })
+
         await User.create({
-            name:"Anupam", // req.body.name -> use it for getting data from user  
-            password:"123",
-            email:"abc12@gmail.com",
-            location:"sada"
-    
+            name:req.body.name,  
+            password:req.body.password,
+            email:req.body.email,
+            location:req.body.location
         })
     res.json({success:true})
     } catch(error){
