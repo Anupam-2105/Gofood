ff0a7983674f65313ae6053709c8928ad89444f9
Anupam Dwivedi <anupamdwivedi2270@gmail.com>
2023-07-15 00:49:56 +0530

    signup

--- backend/Routes/CreateUser.js
+++ backend/Routes/CreateUser.js
@@ -2,8 +2,20 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
 
+const {body,validationResult} = require('express-validator')
+const { toHaveErrorMessage } = require('@testing-library/jest-dom/matchers')
 
-router.post("/createuser", async (req, res) => {
+router.post("/createuser", 
+body('email').isEmail(),
+body('name').isLength({min:5}),
+body('password','Incorrect Password').isLength({min:5}),
+async (req, res) => {
+    
+    const errors = validationResult(res)
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()})
+    }
+    
     try {
         // await User.create({
         //     name:"Anupam", // req.body.name -> use it for getting data from user  
@@ -18,7 +30,6 @@ router.post("/createuser", async (req, res) => {
         //     email: req.body.email,
         //     location: req.body.location
         // })
-
         const { name, password, email, location } = req.body;
         await User.create({ name, password, email, location });
         res.json({ success: true })
